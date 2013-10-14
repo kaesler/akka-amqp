bce1d8598bf9d4b6a5703aaf2fe4ce78c191feee
Kevin Esler <kevin.esler@gmail.com>
2013-10-14 10:08:01 -0400

    Implemented AmqpAdapter.withTempChannel.

--- src/main/scala/akka/amqp/AmqpAdapter.scala
+++ src/main/scala/akka/amqp/AmqpAdapter.scala
@@ -8,6 +8,7 @@ import akka.pattern.ask
 import akka.actor.ActorRef
 import scala.concurrent.Future
 import scala.reflect.ClassTag
+import com.rabbitmq.client.Channel
 
 /**
  * Allows use AMQP without necessitating an Akka extension, which requires
@@ -21,7 +22,21 @@ class AmqpAdapter(settings: AmqpSettings, implicit val _system: ActorSystem) {
   val connectionActor = _system.actorOf(Props(new ConnectionActor(settings, connectionStatusAgent)), "amqp-connection")
 
   def createChannel(): Future[ActorRef] = {
-    implicit val to = akka.util.Timeout(5 seconds)
+    implicit val to = akka.util.Timeout(5.seconds)
     (connectionActor ? CreateChannel()).mapTo[ActorRef]
+  }
+
+  def withTempChannel[T: ClassTag](callback: RabbitChannel ⇒ T): Future[T] = {
+    implicit val to = akka.util.Timeout(5.seconds)
+    val f = connectionActor ? WithConnection { connection =>
+      var channel: Channel = null
+      try {
+        channel = connection.createChannel
+        callback(channel)
+      } finally {
+        channel.close
+      }
+    }
+    f.mapTo[T]
   }
 }
