f99a76a03f8274a6e756afba53fad293666126ae
Kevin Esler <kevin.esler@gmail.com>
2013-10-14 03:47:39 -0400

    Created AmqpAdapter class to remove need for Akka plugin

---
+++ src/main/scala/akka/amqp/AmqpAdapter.scala
@@ -0,0 +1,39 @@
+package akka.amqp
+
+import scala.concurrent.duration._
+import akka.actor.ActorSystem
+import akka.agent.Agent
+import akka.actor.Props
+import akka.pattern.ask
+import akka.actor.ActorRef
+import scala.concurrent.Future
+import scala.reflect.ClassTag
+
+/**
+ * Allows use AMQP without necessitating an Akka extension, which requires
+ * initialization from the config file.
+ */
+class AmqpAdapter(settings: AmqpSettings, implicit val _system: ActorSystem) {
+
+  protected val connectionStatusAgent = Agent(false)(_system)
+  def isConnected = connectionStatusAgent.get
+
+  val connectionActor = _system.actorOf(Props(new ConnectionActor(settings, connectionStatusAgent)), "amqp-connection")
+
+  def createChannel = {
+    implicit val to = akka.util.Timeout(5 seconds)
+    (connectionActor ? CreateChannel()).mapTo[ActorRef]
+  }
+
+  def withTempChannel[T: ClassTag](callback: RabbitChannel ⇒ T): Future[T] = {
+    ???
+    //    withConnection { conn ⇒
+    //      val ch = conn.createChannel()
+    //      try {
+    //        callback(ch)
+    //      } finally {
+    //        if (ch.isOpen) { ch.close() }
+    //      }
+    //    }
+  }
+}
