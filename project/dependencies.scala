f708dd76edefc8ba198713959e0744d323d91ebf
Kevin Esler <kevin.esler@gmail.com>
2015-01-09 11:24:35 -0500

    Updated to Akka-2.3.8, scalatest-2.2.3. Added SCoverage.

--- project/dependencies.scala
+++ project/dependencies.scala
@@ -1,15 +1,12 @@
 import sbt._
 
 object dependencies {
-  def Scalatest    = "org.scalatest" %% "scalatest" % "2.2.0" % "test"   	  // ApacheV2
-  def scalaActorsForScalaTest = "org.scala-lang" % "scala-actors" % "2.11.1" % "test"
-  def AmqpClient = "com.rabbitmq" % "amqp-client" % "2.8.7"   													  // ApacheV2
-
-
-  def AkkaAgent = "com.typesafe.akka" %% "akka-agent" % "2.3.3"
-
-  def Specs2      = "org.specs2"                 % "specs2_2.11"              % "2.3.12"        % "test"  // MIT
-  def JUnit = "junit" % "junit" % "4.7" % "test"   																 // Common Public License 1.0
-  def AkkaTestKit = "com.typesafe.akka" %% "akka-testkit" % "2.3.3" % "test"
-  def Mockito = "org.mockito" % "mockito-all" % "1.9.0" % "test"     											// MIT
+  def Scalatest    = "org.scalatest" %% "scalatest" % "2.2.3" % "test"
+  def scalaActorsForScalaTest = "org.scala-lang" % "scala-actors" % "2.11.4" % "test"
+  def AmqpClient = "com.rabbitmq" % "amqp-client" % "2.8.7"
+  def AkkaAgent = "com.typesafe.akka" %% "akka-agent" % "2.3.8"
+  def Specs2      = "org.specs2"                 % "specs2_2.11"              % "2.3.12"        % "test"
+  def JUnit = "junit" % "junit" % "4.7" % "test"
+  def AkkaTestKit = "com.typesafe.akka" %% "akka-testkit" % "2.3.8" % "test" 
+  def Mockito = "org.mockito" % "mockito-all" % "1.9.0" % "test"
 }
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -4,7 +4,7 @@ resolvers += Classpaths.typesafeResolver
 
 addSbtPlugin("com.typesafe.sbt" % "sbt-scalariform" % "1.2.0")
 
-addSbtPlugin("com.typesafe.sbteclipse" % "sbteclipse-plugin" % "2.4.0")
+addSbtPlugin("com.typesafe.sbteclipse" % "sbteclipse-plugin" % "2.5.0")
 
 addSbtPlugin("com.typesafe.sbt" % "sbt-multi-jvm" % "0.3.8")
 
